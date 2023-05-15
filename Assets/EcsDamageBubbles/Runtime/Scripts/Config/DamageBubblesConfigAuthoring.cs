80a096339a24cd6e22f899efaa79b5b93d7031eb
Nikolay Kandalintsev <nicloay@gmail.com>
2023-05-15 10:36:57 +0200

    remove ingored files

---
+++ Assets/EcsDamageBubbles/Runtime/Scripts/Config/DamageBubblesConfigAuthoring.cs
@@ -0,0 +1,42 @@
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Config
+{
+    public class DamageBubblesConfigAuthoring : MonoBehaviour
+    {
+        public GameObject glyphPrefab;
+        public float verticalOffset = 2f;
+        public float movementTime = 2f;
+        public float scaleOffset = 1f;
+        public float glyphZOffset = 0.001f;
+        public float glyphWidth = 0.07f;
+        public Color[] damageColors; 
+        
+        public class ConfigDataBaker : Baker<DamageBubblesConfigAuthoring>
+        {
+            public override void Bake(DamageBubblesConfigAuthoring authoring)
+            {
+                var entity = GetEntity(TransformUsageFlags.None);
+                AddComponent(entity,new DamageBubblesConfig
+                {
+                    GlyphPrefab = GetEntity(authoring.glyphPrefab, TransformUsageFlags.None),
+                    ScaleOffset = authoring.scaleOffset,
+                    VerticalOffset = authoring.verticalOffset,
+                    MovementTime = authoring.movementTime,
+                    GlyphZOffset = authoring.glyphZOffset,
+                    GlyphWidth = authoring.glyphWidth
+                });
+                
+                var buffer = AddBuffer<DamageBubbleColorConfig>(entity);
+                foreach (var managedColor in authoring.damageColors)
+                {
+                    var color = new float4(managedColor.r, managedColor.g, managedColor.b, managedColor.a);
+                    buffer.Add(new DamageBubbleColorConfig(){Color = color});
+                }
+            }
+        }
+    }
+}
